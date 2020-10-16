12802abc562ddbbbc71f519959d513c1d78c5fc4
Sid <sidneyschiff@gmail.com>
2020-10-16 12:18:44 -0600

    Solves majorityElement

---
+++ Oct16/majorityElement.js
@@ -0,0 +1,44 @@
+/*
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+Example 1:
+
+Input: [3,2,3]
+Output: 3
+Example 2:
+
+Input: [2,2,1,1,1,2,2]
+Output: 2
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+  let obj = {}
+
+  for(let i = 0; i < nums.length; i++) {
+    if (obj[nums[i]] === undefined) {
+      obj[nums[i]] = 1
+    } else {
+      obj[nums[i]]++
+    }
+  }
+  let maj = 0
+  let result
+  for (let num in obj) {
+    if (obj[num] > maj) {
+      maj = obj[num]
+      result = num
+    }
+  }
+
+  return result
+};
+
+// Testing
+
+console.log('3: ', majorityElement([3,2,3]))
