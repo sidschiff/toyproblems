e38f44a718f706a62a016bd39170fd827b084698
Sid <sidneyschiff@gmail.com>
2020-10-27 11:49:45 -0600

    solves sortedArraytoBST

---
+++ Oct27/sortedArraytoBST.js
@@ -0,0 +1,40 @@
+/*
+Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+Example:
+
+Given the sorted array: [-10,-3,0,5,9],
+
+One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function(nums, start = 0, end = nums.length - 1) {
+  if (start <= end){
+      let mid = Math.floor((start + end) / 2)
+      let root = new TreeNode(nums[mid])
+      root.left = sortedArrayToBST(nums, start, mid - 1)
+      root.right = sortedArrayToBST(nums, mid + 1, end)
+      return root
+  }
+  return null
+};
